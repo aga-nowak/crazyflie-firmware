6a8ae4a97f539d3303eacd4df59183a1baba0c3c
Sven Pfeiffer <sven.pfeiffer@hotmail.com>
2023-09-12 12:11:10 +0200

    onboard control for flying to gate

--- src/deck/drivers/src/jevois.c
+++ src/deck/drivers/src/jevois.c
@@ -13,8 +13,20 @@
 #include "task.h"
 #include "system.h"
 #include "eventtrigger.h"
+#include "commander.h"
 
 #define MAX_MESSAGE_SIZE 32 // max no of chars in the message
+
+#define DEFAULT_HEIGHT 1.0f
+#define YAW_BIAS 8.0f;
+
+#define ALPHA_YAW 0.8f
+#define ALPHA_Z 0.8f
+
+#define ERROR_GAIN_Y 3.0f
+#define ERROR_GAIN_X 0.001f
+
+#define TIME_LIMIT 60.0f
 
 static bool isInit = false;
 
@@ -22,6 +34,12 @@ int16_t loggedErrorX = 0;
 int16_t loggedErrorY = 0;
 int16_t loggedWidth = 0;
 int16_t loggedHeight = 0;
+
+static uint8_t start_flight = false;
+
+static float current_yawrate = 0.0f;
+static float current_z_cmd = DEFAULT_HEIGHT;
+static setpoint_t current_setpoint;
 
 void snakeGate()
 {
@@ -83,14 +101,137 @@ void snakeGate()
     loggedHeight = atoi(height);
 }
 
+
+void jevoisInitCommand(){
+    current_setpoint.mode.roll = modeAbs;
+    current_setpoint.mode.pitch = modeAbs;
+    current_setpoint.mode.yaw = modeVelocity;
+
+    current_setpoint.mode.z = modeAbs;
+}
+
+void jevoisSendTakeOffCommand(){
+    current_setpoint.attitude.roll = 0.0f;
+    current_setpoint.attitude.pitch = 0.0;
+    current_setpoint.attitudeRate.yaw = 0.0f;
+    current_setpoint.position.z = DEFAULT_HEIGHT;
+
+    commanderSetSetpoint(&current_setpoint, COMMANDER_PRIORITY_CRTP);
+}
+
+void jevoisSendForwardCommand(){
+    current_setpoint.attitude.roll = 0.0f;
+    current_setpoint.attitude.pitch = -10.0f;
+    current_setpoint.attitudeRate.yaw = 0.0f;
+
+    current_setpoint.position.z = current_z_cmd;
+
+    commanderSetSetpoint(&current_setpoint, COMMANDER_PRIORITY_CRTP);
+
+}
+
+void jevoisSendLandingCommand(){
+    current_setpoint.attitude.roll = 0.0f;
+    current_setpoint.attitude.pitch = 0.0f;
+    current_setpoint.attitudeRate.yaw = 0.0f;
+
+    current_setpoint.position.z = 0.0f;
+
+    commanderSetSetpoint(&current_setpoint, COMMANDER_PRIORITY_CRTP);
+
+}
+
+void jevoisSendGateCommand(int errorX, int errorY){
+    logVarId_t varZid = logGetVarId("stateEstimate", "z");
+    float z = logGetFloat(varZid);
+
+    float new_yaw_rate = ALPHA_YAW * current_yawrate - (1-ALPHA_YAW)*ERROR_GAIN_Y*errorY + YAW_BIAS;
+    float new_z_cmd = ALPHA_Z * current_z_cmd + (1-ALPHA_Z)* (ERROR_GAIN_X*errorX + z);
+
+    if (new_z_cmd > 1.8f){
+        new_z_cmd = 1.8f;
+    }
+
+    current_setpoint.attitude.roll = 0.0f;
+    current_setpoint.attitude.pitch = -5.0;
+    current_setpoint.attitudeRate.yaw = new_yaw_rate;
+    current_setpoint.position.z = new_z_cmd;
+
+    commanderSetSetpoint(&current_setpoint, COMMANDER_PRIORITY_CRTP);
+
+    current_yawrate = new_yaw_rate;
+    current_z_cmd = new_z_cmd;
+}
+
+
 void jevoisTask(void *param)
 {
     systemWaitStart();
 
+    float errorX = 0.0f;
+    float errorY = 0.0f; 
+    float width = 0.0f; 
+    float height = 0.0f;
+
+    while (!start_flight)
+    {
+        vTaskDelay(M2T(100));
+    }
+    
+
+    float time_passed = 0.0f;
+    while (time_passed < 5){
+        if (!start_flight){jevoisSendLandingCommand();}
+        jevoisSendTakeOffCommand();
+        vTaskDelay(M2T(50));
+        time_passed += 0.05f;
+    }
+
+    time_passed = 0.0f;
+    while ((time_passed < TIME_LIMIT) && (width<180) && (height<180))
+    {
+        if (!start_flight){jevoisSendLandingCommand();}
+
+        snakeGate();
+
+        errorX = loggedErrorX;
+        errorY = loggedErrorY;
+        width = loggedWidth;
+        height = loggedHeight;
+        
+        jevoisSendGateCommand(errorX, errorY);
+        
+        vTaskDelay(M2T(50));
+        time_passed += 0.05f;
+    }
+
+    time_passed = 0.0f;
+    while (time_passed < 5){
+        if (!start_flight){jevoisSendLandingCommand();}
+
+        jevoisSendForwardCommand();
+                
+        vTaskDelay(M2T(50));
+        time_passed += 0.05f;
+    }
+
+    time_passed = 0.0f;
+    logVarId_t varZid = logGetVarId("stateEstimate", "z");
+    float z = logGetFloat(varZid);
+    while ((time_passed < 10) && (z > 0.1f)){
+
+        jevoisSendLandingCommand();
+        z = logGetFloat(varZid);
+                
+        vTaskDelay(M2T(50));
+        time_passed += 0.05f;
+    }
+
     while (true)
     {
-        snakeGate();
+        vTaskDelay(M2T(50));
     }
+    
 }
 
 static void jevoisInit()
@@ -126,4 +267,10 @@ LOG_ADD(LOG_INT16, errorx, &loggedErrorX)
 LOG_ADD(LOG_INT16, errory, &loggedErrorY)
 LOG_ADD(LOG_INT16, width, &loggedWidth)
 LOG_ADD(LOG_INT16, height, &loggedHeight)
+LOG_ADD(LOG_FLOAT, yawCmd, &current_yawrate)
+LOG_ADD(LOG_FLOAT, zCmd, &current_z_cmd)
 LOG_GROUP_STOP(jevois)
+
+PARAM_GROUP_START(jevois)
+PARAM_ADD(PARAM_UINT8, startFlight, &start_flight)
+PARAM_GROUP_STOP(jevois)
