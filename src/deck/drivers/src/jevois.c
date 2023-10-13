24927ad5eeeddb928fea486acb5210b99478c60c
Aga Nowak <a.nowak@tudelft.nl>
2023-10-13 14:19:15 +0200

    try to make timestamps work

--- src/deck/drivers/src/jevois.c
+++ src/deck/drivers/src/jevois.c
@@ -11,73 +11,31 @@
 #include "task.h"
 #include "system.h"
 
-#define MAX_MESSAGE_SIZE 32 // max no of chars in the message
+#define MAX_MESSAGE_SIZE 16
 
 static bool isInit = false;
+uint32_t loggedTimestamp = 0;
 
-int16_t loggedErrorX = 0;
-int16_t loggedErrorY = 0;
-int16_t loggedWidth = 0;
-int16_t loggedHeight = 0;
-
-void snakeGate()
+bool jevoisTimestamp()
 {
+    char buf[MAX_MESSAGE_SIZE];
     char c = '0';
-    char errorX[MAX_MESSAGE_SIZE];
-    char errorY[MAX_MESSAGE_SIZE];
-    char width[MAX_MESSAGE_SIZE];
-    char height[MAX_MESSAGE_SIZE];
-
-    while (c != 'x') uart2Getchar(&c);
 
     for (int i = 0; i < MAX_MESSAGE_SIZE; i++)
     {
-        uart2Getchar(&c);
-        if (c == 'y')
+        if (!uart2GetCharWithDefaultTimeout(&c)) break;
+
+        if (c == '\n')
         {
-            errorX[i] = '\0';
-            break;
+            buf[i] = '\0';
+            loggedTimestamp = atoll(buf);
+            return true;
         }
-        errorX[i] = c;
+
+        buf[i] = c;
     }
 
-    for (int i = 0; i < MAX_MESSAGE_SIZE; i++)
-    {
-        uart2Getchar(&c);
-        if (c == 'w')
-        {
-            errorY[i] = '\0';
-            break;
-        }
-        errorY[i] = c;
-    }
-
-    for (int i = 0; i < MAX_MESSAGE_SIZE; i++)
-    {
-        uart2Getchar(&c);
-        if (c == 'h')
-        {
-            width[i] = '\0';
-            break;
-        }
-        width[i] = c;
-    }
-
-    for (int i = 0; i < MAX_MESSAGE_SIZE; i++)
-    {
-        uart2Getchar(&c);
-        if (c == 'e')
-        {
-            height[i] = '\0';
-            break;
-        }
-        height[i] = c;
-    }
-
-    loggedErrorX = atoi(errorX);
-    loggedErrorY = atoi(errorY);
-    loggedWidth = atoi(width);
-    loggedHeight = atoi(height);
+    return false;
 }
 
 void jevoisTask(void *param)
@@ -86,7 +44,7 @@ void jevoisTask(void *param)
 
     while (true)
     {
-        snakeGate();
+        if (jevoisTimestamp()) DEBUG_PRINT("%lu\n", loggedTimestamp);
     }
 }
 
@@ -95,9 +53,9 @@ static void jevoisInit()
     DEBUG_PRINT("Initialize driver\n");
 
     uart2Init(115200);
-    
+
     xTaskCreate(jevoisTask, JEVOIS_TASK_NAME, JEVOIS_TASK_STACKSIZE, NULL, JEVOIS_TASK_PRI, NULL);
-    
+
     isInit = true;
 }
 
@@ -107,9 +65,9 @@ static bool jevoisTest()
 }
 
 static const DeckDriver jevoisDriver = {
-  .name = "jevois",
-  .init = jevoisInit,
-  .test = jevoisTest,
+        .name = "jevois",
+        .init = jevoisInit,
+        .test = jevoisTest,
 };
 
 DECK_DRIVER(jevoisDriver);
@@ -119,8 +77,5 @@ DECK_DRIVER(jevoisDriver);
  * Logging variables for the jevois camera
  */
 LOG_GROUP_START(jevois)
-LOG_ADD(LOG_INT16, errorx, &loggedErrorX)
-LOG_ADD(LOG_INT16, errory, &loggedErrorY)
-LOG_ADD(LOG_INT16, width, &loggedWidth)
-LOG_ADD(LOG_INT16, height, &loggedHeight)
+LOG_ADD(LOG_UINT32, timestamp, &loggedTimestamp)
 LOG_GROUP_STOP(jevois)
