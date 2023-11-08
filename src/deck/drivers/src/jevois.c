6616eedf9e43628c6eb283b1554e6361864fe52f
Aga Nowak <a.nowak@tudelft.nl>
2023-11-08 12:53:09 +0100

    fix whitespace

--- src/deck/drivers/src/jevois.c
+++ src/deck/drivers/src/jevois.c
@@ -19,35 +19,35 @@ uint32_t loggedTimestamp = 0;
 void readSerial() {
     char buf[MAX_MESSAGE_SIZE];
     char c = '0';
-
+    
     for (int i = 0; i < MAX_MESSAGE_SIZE; i++) {
         if (!uart2GetCharWithDefaultTimeout(&c)) break;
-
+        
         if (c == '\n') {
             buf[i] = '\0';
             if (buf[0] >= '0' && buf[0] <= '9') loggedTimestamp = atol(buf);
             else DEBUG_PRINT("%s\n", buf);
             break;
         }
-
+        
         buf[i] = c;
     }
 }
 
 void jevoisTask(void *param) {
     systemWaitStart();
-
+    
     while (1) readSerial();
 }
 
 static void jevoisInit() {
     DEBUG_PRINT("Initialize driver\n");
-
+    
     uart2Init(115200);
-
+    
     xTaskCreate(jevoisTask, JEVOIS_TASK_NAME, JEVOIS_TASK_STACKSIZE, NULL,
                 JEVOIS_TASK_PRI, NULL);
-
+    
     isInit = 1;
 }
 
